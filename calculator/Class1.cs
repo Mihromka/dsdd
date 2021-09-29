c3cadf3539823d12c8f6e9482dae1386fd89ea66
login <mail@yourmail.ru>
2021-09-29 09:21:07 +0300

    init

---
+++ calculator/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class CalculatorChecker
+    {
+        public static bool ValidateCalculator(double sum1, double sum2, string operation, double result)
+        {
+            if (operation == "+")
+            {
+                return (sum1 + sum2 == result);
+            }
+            
+            if(operation == "-")
+            {
+                return (sum1 - sum2 == result);
+
+            }
+            if (operation == "*")
+            {
+                return (sum1 * sum2 == result);
+
+            }
+            if (operation == "/")
+            {
+                return (sum1 / sum2 == result);
+
+            }
+            return true;
+        }
+        
+    }
+}
---
+++ calculatorTests/CalculatorCheckerTests.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.Tests
+{
+    [TestClass()]
+    public class CalculatorCheckerTests
+    {
+        [TestMethod()]
+        public void ValidateCalculatorTest_plus_ReturnTrue()
+        {
+            double a = 2;
+            double b = 4;
+            string operation = "+";
+            double result = 6;
+            bool expected = true;
+            bool actual = CalculatorChecker.ValidateCalculator(a, b, operation,result);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void ValidateCalculatorTest_Plus_ReturnFalse()
+        {
+            double a = 2;
+            double b = 4;
+            double result = 10;
+            bool expected = false;
+            string operation = "+";
+            bool actual = CalculatorChecker.ValidateCalculator(a, b, operation, result);
+            Assert.IsFalse(expected);
+        }
+        [TestMethod]
+        public void ValidateCalculatorTest_Minus_ReturnTrue()
+        {
+            double a = 10;
+            double b = 5;
+            string operation = "-";
+            double result = 5;
+            bool expected = true;
+            bool actual = CalculatorChecker.ValidateCalculator(a, b, operation, result);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void ValidateCalculatorTest_Minus_ReturnFalse()
+        {
+            double a = 10;
+            double b = 2;
+            double result = 9;
+            bool expected = false;
+            string operation = "-";
+            bool actual = CalculatorChecker.ValidateCalculator(a, b, operation, result);
+            Assert.IsFalse(expected);
+        }
+        [TestMethod]
+        public void ValidateCalculatorTest_multiply_ReturnTrue()
+        {
+            double a = 5;
+            double b = 5;
+            string operation = "*";
+            double result = 25;
+            bool expected = true;
+            bool actual = CalculatorChecker.ValidateCalculator(a, b, operation, result);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void ValidateCalculatorTest_multiply_ReturnFalse()
+        {
+            double a = 5;
+            double b = 5;
+            double result = 25;
+            bool expected = false;
+            string operation = "*";
+            bool actual = CalculatorChecker.ValidateCalculator(a, b, operation, result);
+            Assert.IsFalse(expected);
+        }
+        [TestMethod]
+        public void ValidateCalculatorTest_divide_ReturnTrue()
+        {
+            double a = 25;
+            double b = 5;
+            string operation = "/";
+            double result = 5;
+            bool expected = true;
+            bool actual = CalculatorChecker.ValidateCalculator(a, b, operation, result);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void ValidateCalculatorTest_divide_ReturnFalse()
+        {
+            double a = 25;
+            double b = 5;
+            double result = 5;
+            bool expected = false;
+            string operation = "/";
+            bool actual = CalculatorChecker.ValidateCalculator(a, b, operation, result);
+            Assert.IsFalse(expected);
+        }
+
+
+
+
+    }
+}
